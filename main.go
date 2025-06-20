7fba455b3cdfefe0905365f405cd18e5e1cc52e0
stephanevdb <mail@stephanevdb.com>
2025-06-20 15:30:06 +0200

    Remove Docker configuration and static files for PocketBase application

--- main.go
+++ main.go
@@ -4,17 +4,10 @@ import (
 	"log"
 
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
 	app := pocketbase.New()
-
-	// serve static files from the pb_public directory
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.Static("/", "pb_public")
-		return nil
-	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
